c097c8d1b6148725600679036042795bf2f784f9
stepan <sol1222183@gmail.com>
2022-01-18 21:12:20 +0300

    changes

--- King.py
+++ King.py
@@ -1,6 +1,18 @@
 import pygame
+from Appear import load_image
 
 
-class pesh:
-    def __init__(self):
-        pass
+all_sprites = pygame.sprite.Group()
+
+
+class King(pygame.sprite.Sprite):
+    image = load_image("король.png")
+    im2 = load_image("король_ч.png")
+
+    def __init__(self, color, position, *group):
+        super().__init__(*group)
+        self.col = color
+        self.image = King.image if self.col == "white" else King.im2
+        self.rect = self.image.get_rect()
+        self.rect.x = position[0]
+        self.rect.y = position[1]
